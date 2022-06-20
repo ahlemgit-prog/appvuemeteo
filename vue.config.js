0d2eb525aa09329bdf33674d2e603a3f53de4fc8
halouma <essidahlem.info1997@gmail.com>
2022-06-20 02:35:27 +0200

    .

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-const publicPath = process.env.NODE_ENV === 'production' ? '/' : '/'
+const publicPath = process.env.NODE_ENV === 'production' ? '/appvuemeteo/' : '/'
 
 module.exports = {
  publicPath: publicPath,
